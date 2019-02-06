a5379eb5a17b0427af90d11a4154e5fa9d7ed5e0
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-02-06 22:58:26 +0100

    Remove the service worker registration

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 window.addEventListener('load', async _ => {
-  try {
-    await navigator.serviceWorker.register('worker.js');
-    console.log('Registered web worker for transpilation.');
-  } catch (error) {
-    debugger;
-    alert('Error registering a service worker.');
-  }
-
   const inputEditor = monaco.editor.create(document.querySelector('#inputMonacoEditor'), { value: '', language: 'typescript' });
   const outputEditor = monaco.editor.create(document.querySelector('#outputMonacoEditor'), { value: '', language: 'javascript', readOnly: true });
   const timeP = document.querySelector('#timeP');
